a002667cc7c7a03465057107f250a8bcdba74d6a
Georgi Gerganov <ggerganov@gmail.com>
2018-05-12 12:26:12 +0300

    Several fixes:
    - Load larger files by allowing memory growth in emscripten compilation
    - Read files as binary arrays instead of as text
    - Remove unused headers in the C code

--- compile.sh
+++ compile.sh
@@ -2,6 +2,7 @@
 
 echo "static const char * BUILD_TIMESTAMP=\"`date`\";" > build_timestamp.h
 
-em++ -std=c++11 ./main.cpp -o load.js \
+emcc -std=c++11 ./main.cpp -o load.js \
+    -s ALLOW_MEMORY_GROWTH=1 \
     -s EXPORTED_FUNCTIONS='["_setFile", "_main"]' \
-    -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'
+    -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "writeArrayToMemory"]'
--- main.cpp
+++ main.cpp
@@ -9,14 +9,11 @@
 #include "emscripten/emscripten.h"
 #endif
 
-#include <thread>
-#include <algorithm>
-
 extern "C" {
     void setFile(size_t n, const char * buf) {
         printf("Loaded file of size %zu bytes\n", n);
 
-        size_t nPrint = std::min((size_t) 100, n);
+        size_t nPrint = (n < 100) ? n : 100;
         printf("First %zu bytes of the file (0 replaced with '.'):\n", nPrint);
         for (size_t i = 0; i < nPrint; ++i) {
             printf("%c", (buf[i] == 0) ? '.' : buf[i]);
@@ -30,6 +27,7 @@ void update() {
 
 int main(int argc, char** argv) {
     printf("Build time: %s\n", BUILD_TIMESTAMP);
+
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(update, 0, 1);
 #else
